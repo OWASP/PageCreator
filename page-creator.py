af68877435cae01ebd6e0f120e41932ecef9bf0a
hblankenship <harold.blankenship@owasp.org>
2019-09-19 14:43:18 -0400

    run once for the repo names

--- page-creator.py
+++ page-creator.py
@@ -25,51 +25,51 @@ def create_github_repo(github, group, grouptype, msglist):
 
 def main():
 
-    github = OWASPGitHub()
-    group = "deepviolet-tls-ssl-scanner"
-    grouptype = 0
-    r = github.InitializeRepositoryPages(group, grouptype)
-    if github.TestResultCode(r.status_code):
-        r = github.EnablePages(group, grouptype)
-
-    if github.TestResultCode(r.status_code):
-        print("Done")
-    else:
-        print(r.text)
-    # fp = open("projects.txt")
-    
-    # project_type_chapter = 1
-    # project_type_project = 0
-
     # github = OWASPGitHub()
-    # msglist = []
-    # msg = ""
-    # group = fp.readline()
-    # while group:
-    #     group = group.lower().replace("owasp ", "").replace(" project","").replace("\n","")
-    #     msg = "Creating pages for group: %s" % group
-    #     print(msg)
-    #     msglist.append(msg)
-    #     create_github_repo(github, group, project_type_project, msglist)
-    #     group = fp.readline()
-        
-    # fp.close()
+    # group = "deepviolet-tls-ssl-scanner"
+    # grouptype = 0
+    # r = github.InitializeRepositoryPages(group, grouptype)
+    # if github.TestResultCode(r.status_code):
+    #     r = github.EnablePages(group, grouptype)
 
-    # fp = open("chapters.txt")
+    # if github.TestResultCode(r.status_code):
+    #     print("Done")
+    # else:
+    #     print(r.text)
+    fp = open("projects.txt")
     
-    # group = fp.readline()
-    # while group:
-    #     group = group.lower().replace("owasp ", "").replace("chapter","").replace("\n", "")
-    #     msg = "Creating pages for group: %s" % group
-    #     print(msg)
-    #     msglist.append(msg)
-    #     create_github_repo(github, group, project_type_chapter, msglist)
-    #     group = fp.readline()
-        
-    # fp.close()
+    project_type_chapter = 1
+    project_type_project = 0
 
-    # fp = open("creator.log", "w")
-    # fp.writelines(msglist)
-    # fp.close()
+    github = OWASPGitHub()
+    msglist = []
+    msg = ""
+    group = fp.readline()
+    while group:
+        group = group.lower().replace("owasp ", "").replace(" project","").replace("\n","")
+        msg = "Creating pages for group: %s\n" % group
+        print(msg)
+        msglist.append(msg)
+        create_github_repo(github, group, project_type_project, msglist)
+        group = fp.readline()
+        
+    fp.close()
+
+    fp = open("chapters.txt")
+    
+    group = fp.readline()
+    while group:
+        group = group.lower().replace("owasp ", "").replace("chapter","").replace("\n", "")
+        msg = "Creating pages for group: %s\n" % group
+        print(msg)
+        msglist.append(msg)
+        create_github_repo(github, group, project_type_chapter, msglist)
+        group = fp.readline()
+        
+    fp.close()
+
+    fp = open("creator.log", "w")
+    fp.writelines(msglist)
+    fp.close()
     
 main()
